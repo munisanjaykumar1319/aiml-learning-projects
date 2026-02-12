42d8f2bea2e9d565c9a6fd9314d066d2cd7ba061
munisanjaykumar1319 <munisanjaykumar1319@gmail.com>
2026-02-12 22:15:14 +0530

    Create marks_analysis.py

---
+++ student-marks-analysis/marks_analysis.py
@@ -0,0 +1,9 @@
+marks = [78, 85, 92, 67, 88, 74]
+
+average = sum(marks) / len(marks)
+highest = max(marks)
+lowest = min(marks)
+
+print("Average:", average)
+print("Highest:", highest)
+print("Lowest:", lowest)
